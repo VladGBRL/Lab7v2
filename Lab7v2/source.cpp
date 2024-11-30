5df00874f4f06f2a67b958ee31f9abdf6c2cc584
Vlad-Gabriel Bila <bila.vlad.u6i@student.ucv.ro>
2024-11-30 18:00:06 +0200

    o

--- Lab7v2/source.cpp
+++ Lab7v2/source.cpp
@@ -8,33 +8,28 @@
 
 std::mutex mtx;
 std::condition_variable cv;
-int current_type = -1;             // Tipul curent (-1: niciun tip, 0: alb, 1: negru)
-int active_processes = 0;    // Numărul de procese albe active
-   // Numărul de procese negre active
-std::queue<int> request_queue;     // Coada cererilor
+int current_type = -1;          
+int active_processes = 0;  
+std::queue<int> request_queue;   
 
 void accessResource(pid_t pid, int type) {
     std::unique_lock<std::mutex> lock(mtx);
 
-    // Adaugă cererea în coadă
     request_queue.push(type);
 
-    // Așteaptă până când resursa devine disponibilă pentru acest tip
     cv.wait(lock, [&] {
         return (request_queue.front() == type && (current_type == type || current_type == -1));
         });
 
-    // Procesul de același tip poate accesa resursa
     current_type = type;
     active_processes++;
     request_queue.pop();
 
-    // Acces la resursă
     std::cout << "PID " << pid << " (tip " << (type == 0 ? "alb" : "negru")
         << ") accesează resursa.\n";
 
     lock.unlock();
-    sleep(1); // Simulează timpul de utilizare a resursei
+    sleep(1);i
     lock.lock();
 
     std::cout << "PID " << pid << " (tip " << (type == 0 ? "alb" : "negru")
@@ -42,9 +37,8 @@ void accessResource(pid_t pid, int type) {
 
     active_processes--;
 
-    // Dacă nu mai sunt procese active de același tip, permite accesul altui tip
     if (active_processes == 0) {
-        current_type = -1; // Resursa devine disponibilă pentru alt tip
+        current_type = -1; 
         
     }
 
@@ -63,10 +57,9 @@ int main() {
     pid_t pid = fork();
     for (int i = 0; i < num_processes; ++i) {
         
-        createChildProcess(pid,i % 2); // Procesele sunt alternate între alb și negru
+        createChildProcess(pid,i % 2); 
     }
-
-    // Așteaptă finalizarea tuturor proceselor copil
+    =
     for (int i = 0; i < num_processes; ++i) {
         wait(nullptr);
     }
